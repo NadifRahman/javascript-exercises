617fdff3707ef50ff3cde4e147a55af1869235a2
NadifRahman <nadif.rah@gmail.com>
2024-04-26 13:01:12 -0400

    Complete excercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+
+    if(number1 < 0 || typeof number1 !== 'number' || isNaN(number1) || number2 < 0 || typeof number2 !== 'number' || isNaN(number2)) {
+        return 'ERROR';
+    }
+    
+    let bigger;
+    let smaller;
+    let sum = 0;
+
+    if(number1 > number2) {
+        bigger = number1;
+        smaller = number2;
+    }
+    else {
+        bigger = number2;
+        smaller = number1;
+    }
+
+    for (let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
