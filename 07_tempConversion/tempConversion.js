9aba5d637459a742dc84d36dea10c23e5d44cddc
NadifRahman <nadif.rah@gmail.com>
2024-04-26 14:44:48 -0400

    Complete excercise #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  return +((inputTemp - 32) * 5 / 9).toFixed(1); //tofixed returns a string, use + to conver to number
+  //could have also multiplied by 10, rounded it, and then divided by 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+
+return +((9/5 * inputTemp) + 32).toFixed(1);
 };
 
 // Do not edit below this line
