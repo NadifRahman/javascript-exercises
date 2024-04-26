41292c5c669910af52e85dd03cd4beb6d6583a82
NadifRahman <nadif.rah@gmail.com>
2024-04-26 11:39:51 -0400

    Complete excercise #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let outputString = "";
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        outputString = outputString + inputString.charAt(i);
+    }
+    return outputString;
 
 };
 
