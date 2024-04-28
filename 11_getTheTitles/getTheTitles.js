c0ecde39cd4c1fb37d99bd39cd5c2fae48963a57
NadifRahman <nadif.rah@gmail.com>
2024-04-28 17:35:03 -0400

    Complete excercise #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    return titleArray = bookArray.map(bookObj => bookObj.title);
 };
 
 // Do not edit below this line
