09ab675d5c294e978a014471472a2ac56e38fc4e
NadifRahman <nadif.rah@gmail.com>
2024-04-28 17:09:51 -0400

    Complete excercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	 return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  
+	 let result = a.reduce((sum, element) => sum + element, 0);
+   return result;
+
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(a) {
 	
+  if (a <= 1) {
+    return 1;
+  }
+  else {
+    return a * factorial(a-1);
+  }
+  
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (aString) {
+  let finalStringArray = aString.toLowerCase().split('').filter(letter => 
+  (letter.charCodeAt() >= 97 && letter.charCodeAt() <= 122) || (letter.charCodeAt() >= 48 && letter.charCodeAt() <= 57));
 
+  let finalString = finalStringArray.join('');
+    return (finalString === finalStringArray.slice().reverse().join(''));
 };
 
 // Do not edit below this line
